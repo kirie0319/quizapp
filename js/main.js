edc811064628c834f7c3dc99119d4f7f727e963c
kirie0319 <matsueda0407@icloud.com>
2021-06-03 19:48:26 +0900

    I do not  know how to create now

--- js/main.js
+++ js/main.js
@@ -3,20 +3,33 @@
 {
   const startBtn = document.getElementById('start');
   const caption = document.getElementById('caption');
+  const quizData = 'https://opentdb.com/api.php?amount=10';
 
   class Quiz {
+    constructor(quizData) {
+      this._quizzes = quizData.results;
+      this._correctAnswersNum = 0;
+    }
+    getQuizCategory(index) {
+      return this._quizzes[index - 1].category;
+    }
+    resetQuizResults() {
+      this._quizzes.questions =  '';
 
-    constructor(index) {
-      this.index = index;
     }
   }
 
   startBtn.addEventListener('click', () => {
     caption.textContent = '取得中';
-    fetch('https://opentdb.com/api.php?amount=10')
-    .then((response) => response.text())
-    .then((text) => console.log(text))
-    .catch((error) => console.log(error));
+    // fetch('https://opentdb.com/api.php?amount=10')
+    // .then((response) => response.text())
+    // .then((text) => console.log(text))
+    // .catch((error) => console.log(error));
+
+    const response = await fetch('https://opentdb.com/api.php?amount=10');
+    const quizData = await response.json();
+    console.log(quizData);
+
 
     // await fetch('https://opentdb.com/api.php?amount=10', {
     //   headers: {
