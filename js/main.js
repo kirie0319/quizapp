3052c328e0494a71c2ab9be7abebc5510eedee74
kirie0319 <matsueda0407@icloud.com>
2021-06-07 02:03:51 +0900

    revise 3

--- js/main.js
+++ js/main.js
@@ -7,29 +7,29 @@
   const ganre = document.getElementById('ganre');
 
   
+  class Quiz {
+    constructor(quizData) {
+      this._quizzes = quizData.results;
+      this._correctAnswersNum = 0;
+    }
+    getQuizCategory(index) {
+      return this._quizzes[index - 1].category;
+    }
+    resetQuizDate(index) {
+      return this._quizzes[index - 1].question;
+    }
+  }
+  async function callApi(ganre, index) {
+    const response = await fetch('https://opentdb.com/api.php?amount=10');
+    const quizData = await response.json();
+    console.log(quizData);
+    let Quiz1 = new Quiz(quizData);
+    ganre.textContent = Quiz1.getQuizCategory(index);
+    // content.textContent = Quiz1.resetQuizDate(index);
+  }
   startBtn.addEventListener('click', () => {
     caption.textContent = '取得中';
-    async function callApi(ganre, index) {
-      const response = await fetch('https://opentdb.com/api.php?amount=10');
-      const quizData = await response.json();
-      console.log(quizData);
-      class Quiz {
-        constructor(quizData) {
-          this._quizzes = quizData.results;
-          this._correctAnswersNum = 0;
-        }
-        getQuizCategory(index) {
-          return this._quizzes[index - 1].category;
-        }
-        resetQuizDate(index) {
-          return this._quizzes[index - 1].question;
-        }
-      }
-      let Quiz1 = new Quiz(quizData);
-      ganre.textContent = Quiz1.getQuizCategory(index);
-      // content.textContent = Quiz1.resetQuizDate(index);
-    }
-
     callApi(ganre);
+
   });
 }
